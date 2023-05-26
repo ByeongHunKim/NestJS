dda2048aa49b3c05bb381c22aee7d698cfcb6fbc
ByeongHunKim <rkwhr912@naver.com>
2023-05-26 15:34:22 +0900

    feat : 4. user service

---
+++ nest-typeorm-postgres/src/users/users.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+
+/*
+    Explanation :
+
+    @Injectable : to tell NestJS that this is a service
+    @InjectRepository(User) : to tell NestJS that we want to inject the repository of the User entity
+    @Repository(User) : to tell NestJS that this we want to inject the repository of the User entity
+    we are creating a UserService with 5 methods
+        1. findAll
+        2. findOne
+        3. create
+        4. update
+        5. delete
+*/
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  async findOne(id: number): Promise<User> {
+    return this.userRepository.findOne({ where: { id } });
+  }
+
+  async create(user: Partial<User>): Promise<User> {
+    const newUser = this.userRepository.create(user);
+    return this.userRepository.save(newUser);
+  }
+
+  async update(id: number, user: Partial<User>): Promise<User> {
+    await this.userRepository.update(id, user);
+    return this.userRepository.findOne({ where: { id } });
+  }
+
+  async delete(id: number): Promise<void> {
+    await this.userRepository.delete(id);
+  }
+}
