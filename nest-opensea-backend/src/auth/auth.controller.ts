0d71fbf2b4662b8f6e6afd04fc02172326de867e
ByeongHunKim <rkwhr912@naver.com>
2023-06-01 10:34:18 +0900

    feat : auth-swagger
    endpoint/api 로 swagger 접속 -> api try 할 수 있게 수정

--- nest-opensea-backend/src/auth/auth.controller.ts
+++ nest-opensea-backend/src/auth/auth.controller.ts
@@ -7,19 +7,16 @@ import {
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
-/*
-  실제로 클라이언트가 서버로부터 받은 요청을 처리하는 인터페이스 역할
- */
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
-  @Get(':address')
-  async getSignMessage(@Param() params) {
-    const address = params.address;
 
-    if (!/^[0-9a-f-A-F]{40}$/.test(address)) {
+  @Get(':address')
+  async getSignMessage(@Param('address') address: string) {
+    if (!/^[0-9a-fA-F]{40}$/.test(address)) {
       throw new HttpException('wrong address', HttpStatus.BAD_REQUEST);
     }
+    console.log('address', address);
 
     const authRequest = await this.authService.generateAuthRequest(address);
 
